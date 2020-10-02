6ff5662cbf7e97f7ff20f18ce57e5193bc45f320
Nadar Alpenidze <nadaralp16@gmail.com>
2020-10-02 17:18:01 +0300

    - last psuedo i swear

--- CosmosDb.CrudDemo/Controllers/TodoController.cs
+++ CosmosDb.CrudDemo/Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Cosmos.DataInteractionFacade.Data;
 using CosmosDb.CrudDemo.Models;
+using CosmosDb.CrudDemo.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,46 +14,48 @@ namespace CosmosDb.CrudDemo.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
-        private readonly ICosmosRepository<Todo> _cosmosRepository;
+        private readonly ITodoRepository _repository;
 
-        public TodoController(ICosmosRepository<Todo> cosmosRepository)
+        // private readonly ICosmosRepository<Todo> _repository;
+
+        public TodoController(ITodoRepository repository)
         {
-            _cosmosRepository = cosmosRepository;
+            _repository = repository;
         }
 
 
         [HttpGet]
         public async Task<IEnumerable<Todo>> GetTodos()
         {
-            var todos = await _cosmosRepository.GetAllAsync();
+            var todos = await _repository.GetAllAsync();
             return todos;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Todo>> GetTodoById(Guid id)
         {
-            var todo = await _cosmosRepository.GetByIdAsync(id);
+            var todo = await _repository.GetByIdAsync(id);
             return todo;
         }
 
         [HttpPost]
         public async Task<ActionResult> AddTodo([FromBody] Todo todo)
         {
-            await _cosmosRepository.AddSingleAsync(todo);
+            await _repository.AddSingleAsync(todo);
             return Created("", todo);
         }
 
         [HttpPut]
         public async Task<ActionResult> UpdateTodo([FromBody] Todo todo)
         {
-            await _cosmosRepository.UpdateSingleAsync(todo);
+            await _repository.UpdateSingleAsync(todo);
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> AddTodo(Guid id)
         {
-            await _cosmosRepository.DeleteAsync(id);
+            await _repository.DeleteAsync(id);
             return Ok($"item was deleted with id: {id}");
         }
     }
